90ba8665787fe26ae1b01c0abe21a38718da533c
Vicente Eduardo <88383281+VicenteEduardo@users.noreply.github.com>
2023-03-03 10:08:45 +0100

    2

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -27,7 +27,7 @@ class UserFactory extends Factory
             'name' => "admin", //$this->faker->name(),
             'email' => "admin@admin.com", //$this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'username'=>"Vicente500",
+            'username'=>"mario500",
             'level' => 'Administrador-Master',
             'password' => bcrypt("12345678"), //'$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
